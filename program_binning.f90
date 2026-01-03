c15a5758ed0bad321b2ba115c5104c3dd876c53a
itxasoma <itxaso.mam@gmail.com>
2026-01-03 19:59:23 +0100

    program_binning

--- program_binning.f90
+++ program_binning.f90
@@ -1,214 +1,121 @@
-program test
-  use, intrinsic :: iso_fortran_env, only: int64, real64, iostat_end
+PROGRAM TEST
+IMPLICIT NONE
+double precision, allocatable, dimension(:):: energy, magnet
+double precision:: av_E, av_M
+double precision:: sig_E, sig_M
+double precision :: log2
+integer:: i, j, k, MCSTEP, io, idx_start,Nb,i_max
+integer(kind=8):: bb, nlines
+character(len=50):: relleno
+
+! Reads time series files with the MC results and counts lines (MCsteps)
+nlines = 0
+open(55, file = "outputs/timeseries_L20_T2.00_MCSTOT100000000.dat")
+do 
+    read(55,*,IOSTAT=IO)
+    if (IO/=0) EXIT
+    nlines = nlines + 1
+enddo
+close(55)
+
+! Ignores header (first two lines) and store energy and magnetization
+open(55, file = "outputs/timeseries_L20_T2.00_MCSTOT100000000.dat")
+read(55,*) relleno
+read(55,*) relleno
+allocate(energy(nlines-1002))
+allocate(magnet(nlines-1002))
+
+! Ignore first 1000 lines (as requested)
+do i = 1003, nlines
+    read(55,*) MCSTEP, energy(i-1002), magnet(i-1002)
+    ! We are intesested in |M|, not M for the binning
+    magnet(i-1002)=abs(magnet(i-1002))
+enddo
+close(55)
+
+open(88, file = "binning_program_L_20_T_2.0_abs_M.dat")
+
+! Compute maximum block size power from data size
+i_max = floor(log2((nlines-1002)/2.d0))
+do i = 1, i_max
+    bb=2**i
+    sig_E=0.d0
+    sig_M=0.d0
+    call block_analysis(nlines-1002, bb, energy, av_E, sig_E)
+    call block_analysis(nlines-1002, bb, magnet, av_M, sig_M)
+    
+    write(88,*) bb, av_E, sig_E, av_M, sig_M
+enddo
+
+close(88)
+
+
+!   37 | call block_analysis(nlines-1002, 2_int8**i, energy, av_E, sig_E)
+!      |                                        1
+!Error: Missing kind-parameter at (1)
+
+END PROGRAM
+
+double precision function log2(x)
   implicit none
+  double precision, intent(in) :: x
+  log2=log(x)/log(2.d0)
+end function
 
-  ! --------------------
-  ! User parameters
-  ! --------------------
-  character(len=*), parameter :: infile  = "outputs/timeseries_L20_T2.00_MCSTOT100000000.dat"
-  character(len=*), parameter :: outfile = "binning_program_L_20_T_2.0_abs_M.dat"
-  integer(int64),   parameter :: header_lines = 2_int64
-  integer(int64),   parameter :: burnin_valid = 1000_int64   ! ignore first 1000 VALID numeric records
+subroutine block_analysis(ndata, BB, data, avg, sig)
+!   ******* BLOCK AVERAGE METHOD ***************
+!   Intended workflow: (1) run simulation, (2) then make block analyis FOR 1 OBSERVABLE
 
-  ! --------------------
-  ! Data / variables
-  ! --------------------
-  real(real64), allocatable :: energy(:), magnet(:)
-  real(real64) :: av_E, av_M, sig_E, sig_M
-  integer :: i, i_max
-  integer(int64) :: bb
-  integer(int64) :: ndata
+!   Total simulation steps: N
+!   Size of 1 block : BB
+!   Number of blocks : Nb=N/BB
+!   Quantity to sample : m
+!   Block average of quantity m: m_bar = average(m_i in block of size B)
+!   Simulation average of quantity m: <m>=average(m_bar in all blocks)
+!   Simulation error bar of quantity m: sigma(m)
+!   c_0'=mean square dispalcement of each block from the simulation mean
+! **********************************************
+    IMPLICIT NONE
+    integer:: Nb,ib, beg, end
+    integer(kind=8), intent(in):: ndata, BB
+    double precision, intent(in):: data(ndata)
+    double precision, intent(out):: avg, sig
+    double precision:: co
+    double precision, allocatable:: blocks(:)
 
-  integer :: uout
-
-  ! Count how many valid numeric records exist after burn-in
-  ndata = count_valid_records_after_burnin(infile, header_lines, burnin_valid)
-
-  if (ndata <= 1_int64) then
-    error stop "Not enough valid data records after burn-in."
-  end if
-
-  allocate(energy(ndata), magnet(ndata))
-
-  call read_timeseries_after_burnin(infile, header_lines, burnin_valid, energy, magnet)
-
-  open(newunit=uout, file=outfile, status="replace", action="write")
-
-  ! Maximum block size exponent: bb = 2^i, with at least 2 points per block-mean estimate
-  i_max = int( floor( log(real(ndata, real64)/2.0_real64) / log(2.0_real64) ) )
-  if (i_max < 1) i_max = 1
-
-  do i = 1, i_max
-    bb = shiftl(1_int64, i)          ! bb = 2^i as int64
-    sig_E = 0.0_real64
-    sig_M = 0.0_real64
-
-    call block_analysis(ndata, bb, energy, av_E, sig_E)
-    call block_analysis(ndata, bb, magnet, av_M, sig_M)
-
-    write(uout, *) bb, av_E, sig_E, av_M, sig_M
-  end do
-
-  close(uout)
-
-contains
-
-  integer(int64) function count_valid_records_after_burnin(fname, nheader, nburn) result(n_after)
-    use, intrinsic :: iso_fortran_env, only: int64, real64, iostat_end
-    implicit none
-    character(len=*), intent(in) :: fname
-    integer(int64),   intent(in) :: nheader, nburn
-
-    integer :: u, ios
-    character(len=1024) :: line
-    character(len=256)  :: msg
-
-    integer(int64) :: n_valid
-    integer(int64) :: mc
-    real(real64)   :: e, m
-
-    n_after = 0_int64
-    n_valid = 0_int64
-
-    open(newunit=u, file=fname, status="old", action="read", iostat=ios, iomsg=msg)
-    if (ios /= 0) error stop "OPEN failed: " // trim(msg)
-
-    ! Skip header lines (as raw lines)
-    call skip_n_lines(u, nheader)
-
-    do
-      read(u, '(A)', iostat=ios) line
-      if (ios == iostat_end) exit
-      if (ios /= 0) cycle
-
-      line = adjustl(line)
-      if (len_trim(line) == 0) cycle
-      if (line(1:1) == '#') cycle
-
-      read(line, *, iostat=ios) mc, e, m
-      if (ios /= 0) cycle
-
-      n_valid = n_valid + 1_int64
-      if (n_valid > nburn) n_after = n_after + 1_int64
-    end do
-
-    close(u)
-  end function count_valid_records_after_burnin
-
-
-  subroutine read_timeseries_after_burnin(fname, nheader, nburn, energy, magnet)
-    use, intrinsic :: iso_fortran_env, only: int64, real64, iostat_end
-    implicit none
-    character(len=*), intent(in) :: fname
-    integer(int64),   intent(in) :: nheader, nburn
-    real(real64),     intent(out) :: energy(:), magnet(:)
-
-    integer :: u, ios
-    character(len=1024) :: line
-    character(len=256)  :: msg
-
-    integer(int64) :: n_valid, idx
-    integer(int64) :: mc
-    real(real64)   :: e, m
-
-    n_valid = 0_int64
-    idx     = 0_int64
-
-    open(newunit=u, file=fname, status="old", action="read", iostat=ios, iomsg=msg)
-    if (ios /= 0) error stop "OPEN failed: " // trim(msg)
-
-    call skip_n_lines(u, nheader)
-
-    do
-      read(u, '(A)', iostat=ios) line
-      if (ios == iostat_end) exit
-      if (ios /= 0) cycle
-
-      line = adjustl(line)
-      if (len_trim(line) == 0) cycle
-      if (line(1:1) == '#') cycle
-
-      read(line, *, iostat=ios) mc, e, m
-      if (ios /= 0) cycle
-
-      n_valid = n_valid + 1_int64
-      if (n_valid <= nburn) cycle
-
-      idx = idx + 1_int64
-      if (idx > int(size(energy), int64)) exit   ! safety
-
-      energy(idx) = e
-      magnet(idx) = abs(m)
-    end do
-
-    close(u)
-
-    if (idx < int(size(energy), int64)) then
-      ! If fewer records were read than counted (e.g., file changed between passes),
-      ! shrink arrays logically by leaving trailing values unused.
-      ! (Kept simple: just stop to avoid silent mismatch.)
-      error stop "Read fewer valid records than expected (file may have changed)."
+    ! Compute nº of blocks (assumes Nb=10^k and BB up to 2^k or 5^k)
+    Nb = ndata / BB
+    if (Nb.le.1) then
+        print*, "Nb < 2 !!!"
+        avg = sum(data) / dble(ndata)
+        sig = 0.d0
+        return
     end if
-  end subroutine read_timeseries_after_burnin
+    allocate(blocks(Nb))
 
-
-  subroutine skip_n_lines(u, n)
-    use, intrinsic :: iso_fortran_env, only: int64, iostat_end
-    implicit none
-    integer,        intent(in) :: u
-    integer(int64), intent(in) :: n
-    integer(int64) :: k
-    integer :: ios
-    character(len=1024) :: line
-
-    do k = 1_int64, n
-      read(u, '(A)', iostat=ios) line
-      if (ios == iostat_end) exit
+    ! Initialize
+    blocks(:) = 0.d0
+    co  = 0.d0
+    sig = 0.d0
+    avg = 0.d0
+    
+    ! Compute block averages
+    do ib = 1, Nb
+        ! Translate nº of block to samples segment (slicing)
+        beg = 1+BB*(ib-1)
+        end = BB*ib
+        blocks(ib) = sum(data(beg:end))/dble(BB)
     end do
-  end subroutine skip_n_lines
+    ! Compute simulation average
+    avg = sum(blocks(:)) / dble(Nb)
 
-
-  subroutine block_analysis(ndata, bb, data, avg, sig)
-    use, intrinsic :: iso_fortran_env, only: int64, real64
-    implicit none
-    integer(int64), intent(in)  :: ndata, bb
-    real(real64),   intent(in)  :: data(:)
-    real(real64),   intent(out) :: avg, sig
-
-    integer(int64) :: nb, ib
-    integer(int64) :: beg, endd
-    real(real64)   :: co
-    real(real64), allocatable :: blocks(:)
-
-    if (ndata <= 0_int64) then
-      avg = 0.0_real64
-      sig = 0.0_real64
-      return
-    end if
-
-    nb = ndata / bb   ! integer division; leftover tail is ignored (same as your original logic)
-
-    if (nb <= 1_int64) then
-      avg = sum(data(1:int(ndata))) / real(ndata, real64)
-      sig = 0.0_real64
-      return
-    end if
-
-    allocate(blocks(int(nb)))  ! nb should be safe to fit in default int for your sizes
-    blocks = 0.0_real64
-
-    do ib = 1_int64, nb
-      beg  = 1_int64 + bb*(ib-1_int64)
-      endd = bb*ib
-      blocks(int(ib)) = sum(data(int(beg):int(endd))) / real(bb, real64)
+    ! Compute simulation error bar
+    do ib=1,Nb
+        co = co + (blocks(ib)-avg)**2.d0
     end do
-
-    avg = sum(blocks) / real(nb, real64)
-
-    co = sum( (blocks - avg)**2 ) / ( real(nb, real64) * real(nb-1_int64, real64) )
+    co = co / (dble(Nb)*dble(Nb-1))
+    print*, co
+    
+    ! Standard error of the mean
     sig = sqrt(co)
-
-    deallocate(blocks)
-  end subroutine block_analysis
-
-end program test
+end subroutine block_analysis
